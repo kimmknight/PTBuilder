593faf633180e5e07579d5aa50743cff0db8b3ad
Kim Knight <kimknight@gmail.com>
2025-03-26 23:55:35 +1100

    error window bigger

--- source/runcode.js
+++ source/runcode.js
@@ -1,12 +1,16 @@
 function runCode(scriptText) {
-    const codeFunction = new Function(scriptText);
-
     try {
-        codeFunction();
-        return true;
+        const codeFunction = new Function(scriptText);
+        try {
+            codeFunction();
+            return true;
+        } catch (error) {
+            console.log(error);
+            ipc.appWindow().showMessageBox("Builder" + " ".repeat(100), "An error occurred on line " + error.lineNumber + ":", error, 3, 0x00000400, 0x00000400, 0x00000400)
+            return false;
+        }
     } catch (error) {
         console.log(error);
-        ipc.appWindow().showMessageBox("Builder", "An error occurred on line " + error.lineNumber + ":", error, 3, 0x00000400, 0x00000400, 0x00000400)
-        return false;
+        ipc.appWindow().showMessageBox("Builder" + " ".repeat(100), "An error occurred:", error, 3, 0x00000400, 0x00000400, 0x00000400)
     }
 }
